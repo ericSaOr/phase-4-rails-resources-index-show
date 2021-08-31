20d834031bbce68f78ab1e82a16d225c9c1878de
Eric Sanchez <ericfsod@gmail.com>
2021-08-31 11:13:29 -0400

    Completed Assignment

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,16 @@
+class BirdsController < ApplicationController
+    #GET /birds
+    def index
+        birds = Bird.all
+        render json: birds
+    end
+    #Get /birds/:id
+    def show
+        bird = Bird.find_by(id: params[:id])
+        if bird
+            render json: bird
+        else
+            render json: { error: "bird not found" }, status: :not_found
+        end
+    end
+end
